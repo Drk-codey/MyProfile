f8994a7573a1f1380b209283a387fb11a13271de
Kayode Ogunlade <109389152+Drk-codey@users.noreply.github.com>
2023-09-09 23:35:10 +0100

    Add all updated files

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ let currentUTCTime = document.getElementById("currentUTCTime");
 let updateCurrent = setInterval(
   function calcTIme() {
     let date_now = new Date();
-    
+    let utcHours = date_now.getUTCHours();
+    let utcMinutes = date_now.getUTCMinutes();
+    let utcSeconds = date_now.getUTCSeconds();
+
+    utcHours = (utcHours < 10) ? "0" + utcHours : utcHours;
+    utcMinutes = (utcMinutes < 10) ? "0" + utcMinutes : utcMinutes;
+    utcSeconds = (utcSeconds < 10) ? "0" + utcSeconds : utcSeconds;
+
     // Setting for dates
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[date_now.getDay()];
-    let utcHours = date_now.getUTCHours();
-    let utcMinutes = date_now.getUTCMinutes();
-    let utcSeconds = date_now.getUTCSeconds();
-    let utcMilliseconds = date_now.getUTCMilliseconds();
 
     currentDay.textContent = `${day}`;
-    currentUTCTime.textContent = `${utcHours}: ${utcMinutes}: ${utcSeconds}: ${utcMilliseconds}`
+    currentUTCTime.textContent = `${utcHours} : ${utcMinutes} : ${utcSeconds}`
 
   }, 
   1000
