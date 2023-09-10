855ebc55ffbdea13687ec62b3a5f002885dbddb8
Kayode Ogunlade <109389152+Drk-codey@users.noreply.github.com>
2023-09-10 22:14:59 +0100

    Refactor time update code

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 let currentDay = document.getElementById("currentDay");
 let currentUTCTime = document.getElementById("currentUTCTime");
 
+function updateCurrentTime() {
+  let date_now = new Date();
+  let utcHours = date_now.getUTCHours();
+  let utcMinutes = date_now.getUTCMinutes();
+  let utcSeconds = date_now.getUTCSeconds();
 
-let updateCurrent = setInterval(
-  function calcTIme() {
-    let date_now = new Date();
-    let utcHours = date_now.getUTCHours();
-    let utcMinutes = date_now.getUTCMinutes();
-    let utcSeconds = date_now.getUTCSeconds();
+  utcHours = utcHours < 10 ? "0" + utcHours : utcHours;
+  utcMinutes = utcMinutes < 10 ? "0" + utcMinutes : utcMinutes;
+  utcSeconds = utcSeconds < 10 ? "0" + utcSeconds : utcSeconds;
 
-    utcHours = (utcHours < 10) ? "0" + utcHours : utcHours;
-    utcMinutes = (utcMinutes < 10) ? "0" + utcMinutes : utcMinutes;
-    utcSeconds = (utcSeconds < 10) ? "0" + utcSeconds : utcSeconds;
+  // Setting for dates
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date_now.getDay()];
 
-    // Setting for dates
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  currentDay.textContent = `${day}`;
+  currentUTCTime.textContent = `${utcHours}:${utcMinutes}:${utcSeconds}`;
+}
 
-    let day = days[date_now.getDay()];
+// Call the function intially
+updateCurrentTime();
 
-    currentDay.textContent = `${day}`;
-    currentUTCTime.textContent = `${utcHours} : ${utcMinutes} : ${utcSeconds}`
-
-  }, 
-  1000
-);
-
-updateCurrent();
+// Update the time every second
+setInterval(updateCurrentTime, 1000);
